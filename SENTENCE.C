27671aacf2b4d6daa6052a7202904080b25949af
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-02-19 16:34:06 +0530

    Create SENTENCE.C
    print the sentence

---
+++ SENTENCE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+voidmain()
+{
+char a[32];
+int count=0,i;
+gets(a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+count++;
+}
+if("%d",count+1)
+}
